098e5b0c27fe1fa8748999022b61fc4db217716e
Đinh Viết Quân <104368461+Quann111@users.noreply.github.com>
2024-01-29 13:50:39 +0700

    update3

--- QLTL/QLTL/urls.py
+++ QLTL/QLTL/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/archive/',include('archive.urls')),
+    path('api/Docmanager/',include('Docmanager.urls')),
 
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- QLTL/archive/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('bases/', BaseListAPIView.as_view(), name='base-list'),
-    path('bases/<int:pk>/', BaseDetailAPIView.as_view(), name='base-detail'),
-    path('folders/', FolderListAPIView.as_view(), name='folder-list'),
-    path('folders/<int:pk>/', FolderDetailAPIView.as_view(), name='folder-detail'),
-]
